1b224336122ae5ae579e7e93c0aaf1644c11dbe1
rehanshami <77270784+rehanshami@users.noreply.github.com>
2024-12-09 23:42:52 +1300

    feat: Add app image section

--- app/page.tsx
+++ app/page.tsx
@@ -83,6 +83,20 @@ export default function Home() {
             <Link href="/dashboard">Get Started</Link>
           </Button>
         </div>
+        <div className="relative overflow-hidden pt-16">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <Image
+              alt="App screenshot"
+              src="https://i.imgur.com/VciRSTI.jpeg"
+              width={2432}
+              height={1442}
+              className="mb-[0%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+            />
+            <div aria-hidden="true" className="relative">
+              <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]" />
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
