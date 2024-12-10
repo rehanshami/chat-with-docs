2ebb38016dc32d2ec9a266f70d1417566ae40703
rehanshami <77270784+rehanshami@users.noreply.github.com>
2024-12-10 22:53:18 +1300

    feat: Build skeleton layout for Dashboard route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,18 @@
+import Documents from "@/components/Documents";
+
+export const dynamic = "force-dynamic";
+
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="h-full max-w-7xl mx-auto">
+      <h1 className="text-3xl p-5 bg-gray-100 font-extralight text-indigo-600">
+        My documents
+      </h1>
+
+      {/* Documents */}
+      <Documents />
+    </div>
+  );
 }
 
 export default Dashboard;
