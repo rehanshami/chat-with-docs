8b79c59c61c92fbc2232807e162cc7b757790a61
rehanshami <77270784+rehanshami@users.noreply.github.com>
2024-12-16 19:55:32 +1300

    feat: Build PDF view functionality to render PDF

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,11 +1,39 @@
-function ChatToFilePage({
+import PdfView from "@/components/PdfView";
+import { adminDb } from "@/firebaseAdmin";
+import { auth } from "@clerk/nextjs/server";
+
+async function ChatToFilePage({
   params: { id },
 }: {
   params: {
     id: string;
   };
 }) {
-  return <div>Chat to file page: {id}</div>;
+  auth().protect();
+  const { userId } = await auth();
+
+  const ref = await adminDb
+    .collection("users")
+    .doc(userId!)
+    .collection("files")
+    .doc(id)
+    .get();
+
+  const url = ref.data()?.downloadUrl;
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      {/* Right section */}
+      <div className="col-span-5 lg:col-span-2 overflow-y-hidden">
+        {/* Chat */}
+      </div>
+
+      {/* Left section */}
+      <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto">
+        {/* PDF section */}
+        <PdfView url={url} />
+      </div>
+    </div>
+  );
 }
 
 export default ChatToFilePage;
